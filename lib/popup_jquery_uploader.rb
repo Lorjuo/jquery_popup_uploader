ebbe3b6701296b6208c663a16eeb3e9ea26e2bb2
Julien Seitz <julien.seitz@gmail.com>
2015-02-28 11:07:12 +0100

    minor changes

--- lib/popup_jquery_uploader.rb
+++ lib/popup_jquery_uploader.rb
@@ -1,5 +1,9 @@
-require "popup_jquery_uploader/version"
+require 'popup_jquery_uploader'
+require 'popup_jquery_uploader/inputs/attachment_preview_input'
+require 'popup_jquery_uploader/inputs/image_preview_input'
 
 module PopupJqueryUploader
   # Your code goes here...
 end
+
+SimpleForm::Inputs.send(:include, PopupJqueryUploader)
