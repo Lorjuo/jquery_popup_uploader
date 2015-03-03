1efe06bf7d35ce44b034ec8ea90863770bdf10f0
Julien Seitz <julien.seitz@gmail.com>
2015-03-03 14:39:09 +0100

    added id_for tag method

--- lib/popup_jquery_uploader/inputs/image_popup_upload_input.rb
+++ lib/popup_jquery_uploader/inputs/image_popup_upload_input.rb
@@ -1,20 +1,33 @@
 module PopupJqueryUploader::Inputs
-  class ImagePopupUploadInput < SimpleForm::Inputs::FileInput
+  class ImagePopupUploadInput < SimpleForm::Inputs::Base
 
     def input(wrapper_options=nil)
-      version = input_html_options.delete(:preview_version)
-      use_default_url = options.delete(:use_default_url) || false
+      #options array gets filled by wrappers: simple_form/wrappers/many.rb
+
+      # HTML options id etc
+      merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+      
+
+
+      # Plugin specific options
+      default_options = {
+        :preview_version => :thumb,
+        :preview_size => '160x120',
+        :use_default_url => false
+      }
+      opts = default_options.merge(options) # needs to assign output of merging to a variable that is not called "options"
 
       template.content_tag :div, class: 'input-group' do
-        #input = super(wrapper_options) # call superclass method (for rendering real file input field?)
+        debugger
+        input = @builder.hidden_field(attribute_name, merged_input_options)
         
         ac = ActionController::Base.new()
-        ac.render_to_string :partial => "popup_jquery_uploader/field",
+        input += ac.render_to_string :partial => "popup_jquery_uploader/field",
           :locals => {
-            :object => object,
-            :attribute_name => attribute_name,
-            :version => version,
-            :use_default_url => use_default_url}
+            :object => object.send(attribute_name),
+            :options => opts}
+
+        input
       end
     end
 
