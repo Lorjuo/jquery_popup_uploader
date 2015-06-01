6616f963bd0345debaaec99588e6277439d24ace
Julien Seitz <julien.seitz@gmail.com>
2015-06-01 22:36:55 +0200

    minor changes

--- lib/jquery_popup_uploader.rb
+++ lib/jquery_popup_uploader.rb
@@ -1,9 +1,3 @@
-require 'jquery_popup_uploader'
-require 'jquery_popup_uploader/inputs/attachment_preview_input'
-require 'jquery_popup_uploader/inputs/image_preview_input'
-require 'jquery_popup_uploader/inputs/image_popup_upload_input'
-#require 'jquery_popup_uploader/inputs'
-
 module JqueryPopupUploader
   # Your code goes here...
   
@@ -12,5 +6,11 @@ module JqueryPopupUploader
   class Engine < ::Rails::Engine
   end
 end
+
+require 'jquery_popup_uploader'
+require 'jquery_popup_uploader/inputs/attachment_preview_input'
+require 'jquery_popup_uploader/inputs/image_preview_input'
+require 'jquery_popup_uploader/inputs/image_popup_upload_input'
+#require 'jquery_popup_uploader/inputs'
 
 SimpleForm::Inputs.send(:include, JqueryPopupUploader::Inputs)
